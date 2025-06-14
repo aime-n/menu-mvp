d3e9821fadab75ee6b8931d1ed5079da173c2298
aime-n <aime.nobrega@gmail.com>
2025-06-14 20:41:28 -0300

    fastapi structure

--- src/api/main.py
+++ src/api/main.py
@@ -5,8 +5,8 @@ FastAPI app entrypoint.
 - Includes API routes.
 - Runs with uvicorn in dev.
 """
-from api.routers.plan import router as plan_router
-from api.routers.recipes import router as recipes_router
+from src.api.routers.plan import router as plan_router
+from src.api.routers.recipes import router as recipes_router
 from fastapi import FastAPI
 
 app = FastAPI(title="Menu AI MVP Backend", version="1.0.0")
@@ -21,5 +21,5 @@ app.include_router(recipes_router, prefix="/recipes")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="trace")
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
 # Run with: uvicorn src.api.main:app --reload
